8b82715cdb82f361685fa39a2f76b7d58fc648c9
Kanishkpdy <12kanishkpandey12@gmail.com>
2025-11-13 14:35:55 +0530

    Add initial Android Studio project

---
+++ app/app/src/main/java/com/example/real_time_edge_detection_viewer/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.real_time_edge_detection_viewer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.real_time_edge_detection_viewer.ui.theme.Real_Time_Edge_Detection_ViewerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Real_Time_Edge_Detection_ViewerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Real_Time_Edge_Detection_ViewerTheme {
+        Greeting("Android")
+    }
+}
